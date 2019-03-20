5bf4f7bb6951e8b7f456fd0eb2ac97f07dd5b831
gleydisson <gleydisson.bona@gmail.com>
2019-03-20 14:41:21 -0400

    'Cors'

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require("cors");
 const app = express();
 
-app.use(Coordinates());
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
